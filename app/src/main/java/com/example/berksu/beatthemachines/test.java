a124439585e26f7edad397ec5f77cd28be0d3a63
Berksu <berksukismet@gmail.com>
2016-03-15 00:20:47 +0200

    Initial commit

---
+++ app/src/main/java/com/example/berksu/beatthemachines/test.java
@@ -0,0 +1,12 @@
+package com.example.berksu.beatthemachines;
+
+/**
+ * Created by Berksu on 15/03/16.
+ */
+public class test {
+    /**
+     * Created by Berksu on 15/03/16.
+     */
+    //yorum
+    int a=5;
+}
